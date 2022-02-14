5389452b11fb6da7d8c5039066f961c9a424d0f2
KangweiLIAO <Clanceyliao@gmail.com>
2022-02-13 21:20:34 -0500

    utility update

--- Assets/Scripts/PanoramaCapture.cs
+++ Assets/Scripts/PanoramaCapture.cs
@@ -9,22 +9,25 @@ public class PanoramaCapture : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        
+        targetCam.RenderToCubemap(cubemap);
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            // Capture equirect image
+            // Press Space to update the cubemap
             targetCam.RenderToCubemap(cubemap);
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            // Press S to create a panorama jpg file
             cubemap.ConvertToEquirect(equirectRT);
             Texture2D t2d = new Texture2D(equirectRT.width, equirectRT.height);
             RenderTexture.active = equirectRT;
             t2d.ReadPixels(new Rect(0, 0, equirectRT.width, equirectRT.height), 0, 0); // store equirectRT image in a Texture2D
             RenderTexture.active = null;
             byte[] bytes = t2d.EncodeToJPG();
-            string path = Application.dataPath + "/PanoramaCapture" + ".jpg";
+            string path = Application.dataPath + "/PanoramaCapture" + ".jpg"; // save as panorama jpg file
             System.IO.File.WriteAllBytes(path, bytes);
+            Debug.Log("Cubemap successfully captured");
         }
     }
 }
