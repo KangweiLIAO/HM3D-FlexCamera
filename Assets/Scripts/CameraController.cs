d08f3353bfca50b0356c38e58d6b08573ecac6bb
KangweiLIAO <Clanceyliao@gmail.com>
2022-02-15 01:17:13 -0500

    Updates

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour {
     private int currCamIndex = 0;
     private Vector2 rotation = Vector2.zero;
 
+    PanoramaCapture panoramaCapture;
 
     // Start is called before the first frame update
     void Start() {
@@ -14,12 +15,14 @@ public class CameraController : MonoBehaviour {
         Cursor.lockState = CursorLockMode.Locked;
         cameras = Camera.allCameras;
         Debug.Log("Camera Count: " + Camera.allCamerasCount);
+        panoramaCapture = GameObject.Find("Panorama Capture Controller").GetComponent<PanoramaCapture>();
 
         for (int i = 1; i < cameras.Length; i++) {
             cameras[i].gameObject.SetActive(false);
         }
         if (cameras.Length > 0) {
             cameras[0].gameObject.SetActive(true);
+            panoramaCapture.targetCam = cameras[0];
             Debug.Log("Camera with name: " + cameras[0].name + ", is now enabled");
         }
     }
@@ -39,6 +42,7 @@ public class CameraController : MonoBehaviour {
                 cameras[currCamIndex - 1].gameObject.SetActive(false);
                 currCamIndex = 0;
                 cameras[currCamIndex].gameObject.SetActive(true);
+                panoramaCapture.targetCam = cameras[currCamIndex];
                 Debug.Log("Camera with name: " + cameras[currCamIndex].name + ", is now enabled");
             }
         }
--- Assets/Scripts/PanoramaCapture.cs
+++ Assets/Scripts/PanoramaCapture.cs
@@ -1,33 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class PanoramaCapture : MonoBehaviour {
     public Camera targetCam;
-    public RenderTexture cubemap;
-    public RenderTexture equirectRT;
+    public Material cubemapMaterial;
 
     // Start is called before the first frame update
     void Start() {
-        targetCam.RenderToCubemap(cubemap);
+        RenderTexture cubemapRT;
+        // find asset with name Cubemap, type of renderTexture in folder "Assets/Textures":
+        string[] guids = AssetDatabase.FindAssets("Cubemap t:renderTexture", new[] { "Assets/Textures" });
+        foreach (var asset in guids) {
+            // delete the old cubemap textures
+            var path = AssetDatabase.GUIDToAssetPath(asset);
+            AssetDatabase.DeleteAsset(path);
+        }
+        // if cubemap texture not exist, create:
+        cubemapRT = CaptureCubemapTexture(targetCam);
+        cubemapMaterial.SetTexture("_CubeMap", cubemapRT);
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            // Press Space to update the cubemap
-            targetCam.RenderToCubemap(cubemap);
+        if (Input.GetKeyDown(KeyCode.U)) {
+            // Press U to update the cubemap
+            CaptureCubemapTexture(targetCam);
+            Debug.Log("Current cubemap is updated with camera: " + targetCam.name);
         } else if (Input.GetKeyDown(KeyCode.S)) {
             // Press S to create a panorama jpg file
-            cubemap.ConvertToEquirect(equirectRT);
-            Texture2D t2d = new Texture2D(equirectRT.width, equirectRT.height);
-            RenderTexture.active = equirectRT;
-            t2d.ReadPixels(new Rect(0, 0, equirectRT.width, equirectRT.height), 0, 0); // store equirectRT image in a Texture2D
-            RenderTexture.active = null;
-            byte[] bytes = t2d.EncodeToJPG();
-            string path = Application.dataPath + "/PanoramaCapture" + ".jpg"; // save as panorama jpg file
-            System.IO.File.WriteAllBytes(path, bytes);
-            Debug.Log("Cubemap successfully captured");
+            RenderTexture cubemapRT = CaptureCubemapTexture(targetCam);
+            RenderTexture equirectRT = new RenderTexture(4096, 2048, 16);
+            if (equirectRT.Create()) {
+                cubemapRT.ConvertToEquirect(equirectRT);
+                UpdateCubemap(cubemapRT);
+                Texture2D t2d = new Texture2D(equirectRT.width, equirectRT.height); // texture2D to store image data
+                RenderTexture.active = equirectRT;
+                // store equirectRT image in a Texture2D:
+                t2d.ReadPixels(new Rect(0, 0, equirectRT.width, equirectRT.height), 0, 0);
+                RenderTexture.active = null;
+                byte[] bytes = t2d.EncodeToJPG();
+                string path = Application.dataPath + "/Generated/Panorama" + ".jpg"; // save as panorama jpg file
+                System.IO.File.WriteAllBytes(path, bytes);
+                Debug.Log("Cubemap successfully captured");
+            }
         }
+    }
+
+    RenderTexture CaptureCubemapTexture(Camera camera) {
+        RenderTexture cubemap = new RenderTexture(4096, 4096, 32);
+        cubemap.dimension = UnityEngine.Rendering.TextureDimension.Cube;
+        camera.RenderToCubemap(cubemap);
+        if (cubemap.Create()) {
+            AssetDatabase.CreateAsset(cubemap, "Assets/Textures/Cubemap.asset");
+        }
+        return cubemap;
+    }
+
+    void UpdateCubemap(RenderTexture texture) {
+        cubemapMaterial.SetTexture("_CubeMap", texture);
     }
 }
