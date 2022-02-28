55f2a9c2f93a573bf09cee14b8ccb91bceef4902
KangweiLIAO <Clanceyliao@gmail.com>
2022-02-28 15:34:19 -0500

    Cleanup & reload from existing mesh

--- Assets/Scripts/PointSpawnController.cs
+++ Assets/Scripts/PointSpawnController.cs
@@ -12,34 +12,44 @@ public class PointSpawnController : MonoBehaviour {
         if (combineMeshes) {
             // if want to combine meshes under this gameObject
             if (!mf) {
-                // if game object don't have a mesh filter
-                mf = gameObject.AddComponent(typeof(MeshFilter)) as MeshFilter; // create a mesh filter component
+                // create a mesh filter component for root object
+                mf = gameObject.AddComponent<MeshFilter>();
+            }
+            Mesh cm = Resources.Load("CombinedMeshes/" + gameObject.name) as Mesh;
+            if (cm) {
+                // if combined mesh exist assign to root mesh filter
+                mf.mesh = cm;
+                Debug.Log("Combined mesh for " + gameObject.name + " loaded");
+            } else {
                 MeshFilter[] meshFilters = gameObject.GetComponentsInChildren<MeshFilter>();
                 CombineInstance[] combine = new CombineInstance[meshFilters.Length];
                 for (int i = 0; i < meshFilters.Length; i++) {
-                    combine[i].mesh = meshFilters[i].sharedMesh;
-                    combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
-                    i++;
+                    if (meshFilters[i].sharedMesh) {
+                        combine[i].mesh = meshFilters[i].sharedMesh;
+                        combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+                    }
                 }
-                mf.mesh = new Mesh();   // create a new mesh to apply combined mesh
-                // set UInt32 to contains large scale of verties:
-                mf.mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32; 
-                mf.mesh.CombineMeshes(combine);
-                CreateCombinedMeshAssets(mf.mesh);
+                if (combine.Length > 0) {
+                    mf.mesh = new Mesh();   // create a new mesh to apply combined mesh
+                    // set UInt32 to contains large scale of verties:
+                    mf.mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+                    mf.mesh.CombineMeshes(combine);
+                    CreateCombinedMeshAssets(mf.mesh);
+                }
             }
         }
     }
 
     void CreateCombinedMeshAssets(Mesh mesh) {
         // find asset with name CombinedMesh in folder "Assets/Generated":
-        string[] guids = AssetDatabase.FindAssets(gameObject.name, new[] { "Assets/Resources/CombinedMesh/" });
+        string[] guids = AssetDatabase.FindAssets(gameObject.name, new[] { "Assets/Resources/CombinedMeshes/" });
         foreach (var asset in guids) {
             // delete the old cubemap textures
             var path = AssetDatabase.GUIDToAssetPath(asset);
             AssetDatabase.DeleteAsset(path);
         }
-        AssetDatabase.CreateAsset(mesh, "Assets/Resources/CombinedMesh/" + gameObject.name + ".mat");
-        Debug.Log(AssetDatabase.GetAssetPath(mesh));    // Print the path of the saved asset
+        AssetDatabase.CreateAsset(mesh, "Assets/Resources/CombinedMeshes/" + gameObject.name + ".mat");
+        //Debug.Log(AssetDatabase.GetAssetPath(mesh));    // Print the path of the saved asset
     }
 
     void SpawnPoints(Mesh mesh) {
